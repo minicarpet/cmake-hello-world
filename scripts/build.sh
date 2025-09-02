dfc333f4ba75a9fd27a7117e8a6834ce939855bf
Logan Gallois <logan.gallois@hexagon.com>
2025-09-02 15:54:37 +0200

    Create a script build

---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+mkdir $SCRIPT_DIR/../build
+cmake -S $SCRIPT_DIR/.. -B $SCRIPT_DIR/../build
+cmake --build $SCRIPT_DIR/../build --config Release
