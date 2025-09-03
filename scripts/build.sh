e260f3c3f53bc45fcd855bc4ae925957e3c0edc8
Logan Gallois <logan.gallois@hexagon.com>
2025-09-03 10:48:38 +0200

    fix mkdir error

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-mkdir $SCRIPT_DIR/../build
+mkdir -p $SCRIPT_DIR/../build
 cmake -S $SCRIPT_DIR/.. -B $SCRIPT_DIR/../build
 cmake --build $SCRIPT_DIR/../build --config Release
