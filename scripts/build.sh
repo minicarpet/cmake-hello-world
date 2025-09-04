5f97ffe0621307f3fda4073e0999938573c18f16
Logan Gallois <logan.gallois@hexagon.com>
2025-09-04 08:51:28 +0200

    update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 mkdir -p $SCRIPT_DIR/../build
-cmake -S $SCRIPT_DIR/.. -B $SCRIPT_DIR/../build
+cmake -S $SCRIPT_DIR/.. -B $SCRIPT_DIR/../build --fresh
 cmake --build $SCRIPT_DIR/../build --config Release
