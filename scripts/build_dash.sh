feec7979422a77b263b771ff23c623d8bb25cec9
Logan Gallois <logan.gallois@hotmail.fr>
2025-09-17 22:51:54 +0200

    Update build_dash.sh

--- scripts/build_dash.sh
+++ scripts/build_dash.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 echo $SDKTARGETSYSROOT
 
-mkdir -p $SCRIPT_DIR/../build
-cmake -S $SCRIPT_DIR/.. -B $SCRIPT_DIR/../build --fresh
-cmake --build $SCRIPT_DIR/../build --config Release
+mkdir -p ../build
+cmake -S .. -B ../build --fresh
+cmake --build ../build --config Release
